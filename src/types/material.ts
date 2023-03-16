dbaa6d28e72ec4d6517bc44d861dbb9d443c1ec1
yaokaisei <kaisei.design@gmail.com>
2023-03-17 00:31:59 +0900

    マテリアルキーの型追加

--- src/types/material.ts
+++ src/types/material.ts
@@ -1,6 +1,23 @@
 /**
  * マテリアルを構成する部品名
  */
+export type MaterialKey =
+  | 'foxing'
+  | 'healOverlay'
+  | 'heal'
+  | 'quarter'
+  | 'sole'
+  | 'quarterOverlay'
+  | 'tip'
+  | 'vamp'
+  | 'label'
+  | 'logo'
+  | 'eyestay'
+  | 'swoosh'
+  | 'midsole'
+  | 'laces'
+  | 'tongue';
+
 export type MaterialName =
   | 'Foxing'
   | 'Heal Overlay'
